48bf04ed1f7c67dd3a06c6ead602a4d9b74951a1
Juha Viitanen <viidakkovekara@gmail.com>
2023-01-15 09:01:25 +0000

    Working bashport with IoT-ticket support

--- bashport.sh
+++ bashport.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+. ./credentials
 
 timestamp=$(mktemp)
+#echo $username
+#echo $password
+
+function toWapice {
+  #echo $username
+  #echo 1 $1
+  #echo 2 $2
+  #echo 3 $3
+  #echo 4 $4
+  #echo 5 $5
+  #data="[{\"name\": \"$1\",\"path\": \"Mariankatu5/electricity\",\"v\": val,\"ts\": $3,\"unit\": \"sdf\"}]"
+  data="[{\"name\": \"$1-2\",\"path\": \"Consumption\",\"v\": $2,\"unit\": \"$4\",\"dataType\":\"double\", \"ts\": $3000}]"
+  #data="{\"name\": \"$1\",\"path\": \"Consumption\",\"v\": $2,\"unit\": \"$4\"}"
+  echo  $data
+  curl -X POST --user $username:$password -H "Content-Type:application/json" -d "$data" "https://my.iot-ticket.com/api/v1/process/write/$deviceid/"
+}
 
 function conv2JSON {
-  pre_value=$(echo $1| cut -d "(" -f 2|sed 's/)//')
+  #echo $1
+  path=$(echo -n $1| cut -d "(" -f 1)
+  units=$(echo -n $1| cut -d "*" -f 2|sed 's/)//')|tr '\n' ' '
+  pre_value=$(echo -n $1| cut -d "(" -f 2|sed 's/)//')
   value=$(awk -v v="${pre_value}" 'BEGIN{print v+0}')
-  datetime=$(cat $timestamp|sed -E "s|([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2}).+|\3/\2/20\1 \4:\5:\6|")
-  epoch=$(date -d "$datetime" +"%s")
-  echo "{\"ts\":$epoch,\"val\":$value}"
+  datetime=$(cat $timestamp|sed -E "s|([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2}).+|\2/\3/20\1 \4:\5:\6|")
+  echo $datetime
+  epoch=$(date -d "$datetime + 3 hours" +"%s")
+  toWapice $path $value $epoch $units
+  #echo "{\"ts\":$epoch,\"val\":$value}"
 }
 
 input="/dev/ttyUSB0"
@@ -25,13 +47,19 @@ do
     "!"*)
       echo "end" > /dev/null
     ;;
+    #*)
+    #  conv2JSON $line current
+    #;;
     "1-0:1.7.0"*)
-      value=$(conv2JSON $line)
-      mosquitto_pub -r -t aidan/wats -m "${value}"
+      #(
+      conv2JSON $line current
+      #)&
+      #toWapice $value
+      #mosquitto_pub -r -t aidan/wats -m "${value}" &
     ;;
     "1-0:1.8.0"*)
-      value=$(conv2JSON $line)
-      mosquitto_pub -r -t aidan/total -m "${value}"
+      conv2JSON $line total
+      #mosquitto_pub -r -t aidan/total -m "${value}" &
     ;;
     esac
 fi
